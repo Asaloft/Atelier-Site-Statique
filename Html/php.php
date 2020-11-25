286b3f1407d69d7103396569d37e955b9ced9690
Asaloft <75014914+Asaloft@users.noreply.github.com>
2020-11-25 10:36:45 +0100

    Add files via upload

---
+++ Html/php.php
@@ -0,0 +1,7 @@
+<?php
+					echo "Name:" .$_POST["name"]  ;
+					echo "Credit Card:" .$_CCnum  ;
+					echo "Expiration date:" .$_date  ;
+					echo "Cryptogramme:" .$_crypto  ;
+					echo "You didn't really did this, right?" ;
+?>
